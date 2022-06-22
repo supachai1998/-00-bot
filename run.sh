62a1005fd86a0fb6e4d31ab71e042c6603e248de
supcahai <supcahai_thng@hotmail.com>
2022-06-22 10:31:36 +0700

    delay

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ do
     # LOGIN2="user:at123457:1*3*5*" LINE_API2="SRiDgJf3Dz80Fsou3O5fs0tnIpGIaq2OFiRambwpfsv"  \
     LOGIN1="user:at123456:1*3*5*" MODE_AMAZON1="true" LINE_API1="SRiDgJf3Dz80Fsou3O5fs0tnIpGIaq2OFiRambwpfsv" \
     yarn start
-    RR=$((10+RANDOM % (150-10)))
+    RR=$((60+RANDOM % (150-60)))
     echo "Restarting... $RR seconds"
     sleep $RR
 done
