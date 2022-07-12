1a4c7c8c3d9ad88119eb5102fc06a5a648121298
supcahai <supcahai_thng@hotmail.com>
2022-07-12 16:06:32 +0700

    usr 6

--- run.sh
+++ run.sh
@@ -3,11 +3,13 @@
 # MODE_AMAZON[boolean]
 # MODE_ADVENTURE[boolean]
 # LINE_API[string]
+
 yarn go
 while true
 do
     # LOGIN2="user:at123457:1*3*5*" LINE_API2="SRiDgJf3Dz80Fsou3O5fs0tnIpGIaq2OFiRambwpfsv"  \
-    LOGIN1="user:at123456:1*3*5*" MODE_AMAZON1="true" LINE_API1="SRiDgJf3Dz80Fsou3O5fs0tnIpGIaq2OFiRambwpfsv" \
+    # LOGIN1="user:at123457:1*3*5*"  MODE_AMAZON1="false" LINE_API1="SRiDgJf3Dz80Fsou3O5fs0tnIpGIaq2OFiRambwpfsv" \
+    LOGIN1="user:BobbyScholar:BobbyH" LINE_API1="SRiDgJf3Dz80Fsou3O5fs0tnIpGIaq2OFiRambwpfsv" \
     yarn start
     RR=$((60+RANDOM % (150-60)))
     echo "Restarting... $RR seconds"
